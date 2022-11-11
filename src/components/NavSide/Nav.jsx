db9d88844a16c27cffac19171757df392ab2b69b
Eliberto Subias <eliberto.subias@coppel.com>
2022-11-10 19:30:12 -0700

    Corrección de menu movil

--- src/components/NavSide/Nav.jsx
+++ src/components/NavSide/Nav.jsx
@@ -33,10 +33,10 @@ export default function Nav({ cambiarOpcion, opcion, filtrarPorNombre, ordenMeno
                     <Link to="/home" className="navbar-brand" href="#">
                         <img style={{with: "50px", height: "50px"}} src={logo} alt="" />
                     </Link>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <div className="collapse navbar-collapse" id="navbarNav">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
                                 <Link to="/encartelera" onClick={() => setOpcionActiva("encartelera")} className={ `nav-link ${opcion == "encartelera" ? "active" : ""}` } href="#">En Cines</Link>
@@ -48,16 +48,17 @@ export default function Nav({ cambiarOpcion, opcion, filtrarPorNombre, ordenMeno
                                 <Link to="/top" onClick={() => setOpcionActiva("top")} className={ `nav-link ${opcion == "top" ? "active" : ""}` } href="#">Peliculas Top</Link>
                             </li>
                         </ul>
-                        <form className="d-flex" role="search">
-                            <ul className="navbar-nav me-auto mb-2 mb-lg-0 border rounded">
-                                <li className="nav-item nav-link">
+                        <div className="mr-5">
+                            <div className="row me-auto mb-2 mb-lg-0 border rounded">
+                                <div className="col nav-item nav-link p-1">
                                     <FontAwesomeIcon style={{ cursor: "pointer" }} onClick={() => ordenarPorMenor()} color="white" icon={faStarHalf} />
-                                </li>
-                                <li className="nav-item nav-link">
+                                </div>
+                                <div className="col nav-item nav-link p-1">
                                     <FontAwesomeIcon style={{ cursor: "pointer" }} onClick={() => ordenarPorMayor()} color="white" icon={faStar} />
-                                </li>
-                            </ul>
-
+                                </div>
+                            </div>
+                        </div>
+                        <form className="d-flex" role="search">
                             <i className="fa fa-star" aria-hidden="true">2</i>
                             <input value={enteredText} onChange={(e) => buscarPorNombre(e)} className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
                             <button className="btn btn-outline-success" type="submit">Search</button>
